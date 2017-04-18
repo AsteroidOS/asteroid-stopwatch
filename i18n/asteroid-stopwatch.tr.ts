b97d497b6e327647c3c8047bc24a280c98f9dd49
E T <etcetin@gmail.com>
2017-04-18 08:48:56 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (1 of 1 strings)
    
    Translation: AsteroidOS/Stopwatch
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/stopwatch/tr/

--- i18n/asteroid-stopwatch.tr.ts
+++ i18n/asteroid-stopwatch.tr.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="asteroid-stopwatch.desktop.h" line="5"/>
         <source>Stopwatch</source>
-        <translation type="unfinished"></translation>
+        <translation>Süreölçer</translation>
     </message>
 </context>
 </TS>
