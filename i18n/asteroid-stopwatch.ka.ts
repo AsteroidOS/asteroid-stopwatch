ae51112a70dbeb3c3def30e6ea94d40533ee0c67
sam bowlby <sam@chven.us>
2017-11-09 06:53:43 +0000

    Added translation using Weblate (Georgian)

---
+++ i18n/asteroid-stopwatch.ka.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ka">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="asteroid-stopwatch.desktop.h" line="5"/>
+        <source>Stopwatch</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
