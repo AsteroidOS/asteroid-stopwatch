b1e536d8c2e976a522b0ce91619ff3083c4e98b5
Florent Revest <revestflo@gmail.com>
2017-11-25 00:15:46 +0100

    Add a default english translation

---
+++ i18n/asteroid-stopwatch.en_GB.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_GB">
+<context>
+    <name></name>
+    <message id="id-app-launcher-name">
+        <location filename="asteroid-stopwatch.desktop.h" line="6"/>
+        <source>Stopwatch</source>
+        <translation>Stopwatch</translation>
+    </message>
+</context>
+</TS>
