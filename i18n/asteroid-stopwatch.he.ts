d66b5674bac40aaaaeb6d5743557f63d71eddc19
Yaron Shahrabani <sh.yaron@gmail.com>
2017-11-25 13:27:56 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (1 of 1 strings)
    
    Translation: AsteroidOS/Stopwatch
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/stopwatch/he/

--- i18n/asteroid-stopwatch.he.ts
+++ i18n/asteroid-stopwatch.he.ts
@@ -6,7 +6,7 @@
     <message id="id-app-launcher-name">
         <location filename="asteroid-stopwatch.desktop.h" line="6"/>
         <source>Stopwatch</source>
-        <translation></translation>
+        <translation>שעון עצר</translation>
     </message>
 </context>
 </TS>
