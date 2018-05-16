6389f950b46af49c99167d221797eb10a6484df1
Goudarz Jafari <goudarz.jafari@gmail.com>
2018-05-16 13:11:51 +0000

    Translated using Weblate (Persian)
    Currently translated at 100.0% (1 of 1 strings)
    
    Translation: AsteroidOS/Stopwatch
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/stopwatch/fa/

--- i18n/asteroid-stopwatch.fa.ts
+++ i18n/asteroid-stopwatch.fa.ts
@@ -6,7 +6,7 @@
     <message id="id-app-launcher-name">
         <location filename="asteroid-stopwatch.desktop.h" line="6"/>
         <source>Stopwatch</source>
-        <translation>کﺭﻮﻧﻮﻤﺗﺭ</translation>
+        <translation>کرونومتر</translation>
     </message>
 </context>
 </TS>
