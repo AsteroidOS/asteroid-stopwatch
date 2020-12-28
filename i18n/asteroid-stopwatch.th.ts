10fdbfcbabf28f975f7d392e25de121431c508d0
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-12-28 11:09:37 +0000

    Translated using Weblate (Thai)
    Currently translated at 100.0% (1 of 1 strings)
    
    Translation: AsteroidOS/Stopwatch
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/stopwatch/th/

--- i18n/asteroid-stopwatch.th.ts
+++ i18n/asteroid-stopwatch.th.ts
@@ -6,7 +6,7 @@
     <message id="id-app-launcher-name">
         <location filename="asteroid-stopwatch.desktop.h" line="6"/>
         <source>Stopwatch</source>
-        <translation type="unfinished"></translation>
+        <translation>นาฬิกาจับเวลา</translation>
     </message>
 </context>
 </TS>
