30478a191f1d64a0e79b299c0f4e9c7dafb9dfd3
Software In Interlingua <softinterlingua@gmail.com>
2024-01-25 12:16:41 +0000

    Translated using Weblate (Interlingua)
    Currently translated at 100.0% (1 of 1 strings)
    
    Translation: AsteroidOS/Stopwatch
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/stopwatch/ia/

--- i18n/asteroid-stopwatch.ia.ts
+++ i18n/asteroid-stopwatch.ia.ts
@@ -6,7 +6,7 @@
     <message id="id-app-launcher-name">
         <location filename="asteroid-stopwatch.desktop.h" line="6"/>
         <source>Stopwatch</source>
-        <translation type="unfinished"></translation>
+        <translation>Chronometro</translation>
     </message>
 </context>
 </TS>
