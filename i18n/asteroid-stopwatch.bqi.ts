f61ca3a7898c8785bfd2818ef54c55c05cb46403
hosêyň abāspanā <hosseinabaspanah@gmail.com>
2025-06-30 06:30:48 +0200

    Translated using Weblate (Luri (Bakhtiari))
    Currently translated at 100.0% (1 of 1 strings)
    
    Translation: AsteroidOS/Stopwatch
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/stopwatch/bqi/

--- i18n/asteroid-stopwatch.bqi.ts
+++ i18n/asteroid-stopwatch.bqi.ts
@@ -6,7 +6,7 @@
     <message id="id-app-launcher-name">
         <location filename="asteroid-stopwatch.desktop.h" line="6"/>
         <source>Stopwatch</source>
-        <translation type="unfinished"></translation>
+        <translation>زمووݩ سنج</translation>
     </message>
 </context>
 </TS>
