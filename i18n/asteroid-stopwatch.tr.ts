f2cf7e71bcad410a6ed6d9728361b7ffa822d7d4
Çağhan Demir <dmrcghn@gmail.com>
2026-01-28 02:08:50 +0100

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (1 of 1 strings)
    
    Translation: AsteroidOS/Stopwatch
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/stopwatch/tr/

--- i18n/asteroid-stopwatch.tr.ts
+++ i18n/asteroid-stopwatch.tr.ts
@@ -6,7 +6,7 @@
     <message id="id-app-launcher-name">
         <location filename="asteroid-stopwatch.desktop.h" line="6"/>
         <source>Stopwatch</source>
-        <translation>Süreölçer</translation>
+        <translation>Kronometre</translation>
     </message>
 </context>
 </TS>
